ad006f6243cbbb22e46a4095da9dcc4f9ca7c076
daniel bui <daniel-bui@hotmail.com>
2022-03-01 11:27:26 -0500

    added the routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :pets do
     resources :posters, only: %i[new create]
   end
+  resources :posters, only: %i[edit update show]
   get '/my_pets', to: 'pets#my_pets'
-
+  get 'posters/:poster_id/print', to: 'posters#print'
+  get 'posters/:poster_id/found', to: 'posters#mark_as_found'
+  resources :reports, except: %i[destroy]
 end
