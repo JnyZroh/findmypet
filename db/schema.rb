be8f7f85677fa89c2492ad011a649d5ee416828c
Janaya Gripper <jnygripper@gmail.com>
2022-03-02 12:30:44 -0500

    enabled user to submit a report: created a form, modified the schema and seeds, then drafted a view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_212127) do
+ActiveRecord::Schema.define(version: 2022_03_02_170333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 2022_02_28_212127) do
     t.string "address"
     t.string "species"
     t.text "description"
-    t.string "type"
+    t.string "report_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
