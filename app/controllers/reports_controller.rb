006bc641aca8edbe859b2b8ac644211962dc54bf
Janaya Gripper <jnygripper@gmail.com>
2022-03-03 12:43:03 -0500

    added photo input to report form and added image to report show page

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -29,7 +29,7 @@ class ReportsController < ApplicationController
   private
 
   def report_params
-    params.require(:report).permit(:date_spotted, :address, :species, :description, :report_type, :photos [])
+    params.require(:report).permit(:report_type, :species, :date_spotted, :address, :description, photos: [])
   end
 
 end
