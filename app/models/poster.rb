da6cab74c07fceca6959a7a212d5085045dd84a6
daniel bui <daniel-bui@hotmail.com>
2022-03-04 11:49:10 -0500

    linked geocoder to posters controller

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -5,4 +5,7 @@ class Poster < ApplicationRecord
   validates :address, presence: true
 
   accepts_nested_attributes_for :pet
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_234336) do
+ActiveRecord::Schema.define(version: 2022_03_04_163820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,15 +43,6 @@ ActiveRecord::Schema.define(version: 2022_03_03_234336) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "bestfriends", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.float "latitude"
-    t.float "longitude"
-  end
-
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "species"
@@ -75,6 +66,8 @@ ActiveRecord::Schema.define(version: 2022_03_03_234336) do
     t.bigint "pet_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["pet_id"], name: "index_posters_on_pet_id"
   end
 
