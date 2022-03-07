c887f0248949eac7be89154cb1ca75f062b3c8e8
daniel bui <daniel-bui@hotmail.com>
2022-03-07 17:19:31 -0500

    map on index page

---
+++ app/javascript/controllers/lostpets_map_controller.js
@@ -0,0 +1,43 @@
+import { Controller } from "@hotwired/stimulus";
+import mapboxgl from "mapbox-gl";
+
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers_report: Array,
+    markers_posters: Array
+  }
+
+  connect() {
+    mapboxgl.accessToken = this.apiKeyValue;
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/dangne/cl0h23ek0000l14qvlwbirb0e"
+    });
+    this._addMarkersToMap();
+    this._fitMapToMarkers();
+  }
+  _addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+
+      const customMarker = document.createElement("div")
+      customMarker.className = "marker"
+      customMarker.style.backgroundImage = `url('${marker.image_url}')`
+      customMarker.style.backgroundSize = "contain"
+      customMarker.style.width = "40px"
+      customMarker.style.height = "40px"
+
+      new mapboxgl.Marker(customMarker)
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(this.map);
+    });
+  }
+  _fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 2000 })
+  }
+}
