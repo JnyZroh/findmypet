049cfe974a18f7455f598db4ccf52a77485407ca
daniel bui <daniel-bui@hotmail.com>
2022-03-07 11:07:17 -0500

    fixed the icon and popup

--- app/controllers/posters_controller.rb
+++ app/controllers/posters_controller.rb
@@ -26,8 +26,8 @@ class PostersController < ApplicationController
     @markers =
     [{
       lat: @poster.latitude,
-      lng: @poster.longitude
-      #info_window: render_to_string(partial: "info_window", locals: { poster: @poster })
+      lng: @poster.longitude,
+      info_window: render_to_string(partial: "info_window", locals: { poster: @poster }),
       image_url: helpers.asset_url("peticon.png")
       }]
     # #@markers = @poster.geocoded.map do |poster|
