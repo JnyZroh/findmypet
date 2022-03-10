098329b0717a441d788778496e7b90a31e0c8785
Rabih Mteirek <rabihmteyrek@hotmail.com>
2022-03-10 15:43:48 -0500

    Esteban fixes

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Findmypet
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.time_zone = "America/Montreal"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
