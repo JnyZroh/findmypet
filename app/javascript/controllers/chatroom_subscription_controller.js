7d020b660bd514bbf810486e45d73e29176bb760
Rabih Mteirek <rabihmteyrek@hotmail.com>
2022-03-11 09:52:58 -0500

    Fixed chat reset

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -21,20 +21,17 @@ export default class extends Controller {
     var htmlObject = document.createElement('div');
     htmlObject.innerHTML = data;
     var firstDiv = htmlObject.firstChild.nextSibling;
-    console.log(firstDiv.id);
-    console.log(this.userIdValue);
 
     if (firstDiv.id != this.userIdValue) {
-      console.log(data);
       var lines = data.split("\n")
       lines[0] = "  <div>"
       lines[1] = "    <div class=\"m-1 p-2 gray-bubble\" style=\"max-width: 400px;\">"
       data = lines.join("\n")
-      console.log(data);
+    }else{
+      this.formTarget.reset()
     }
 
     this.messagesTarget.insertAdjacentHTML("beforeend", data)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
-    this.formTarget.reset()
   }
 }
